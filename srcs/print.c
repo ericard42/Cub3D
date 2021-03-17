73dbf6a076da813640cbd88e8dcb3863ed7799fe
ericard42 <ericard@student.42.fr>
2021-03-17 14:42:15 +0100

    start fixing texture

--- srcs/print.c
+++ srcs/print.c
@@ -6,7 +6,7 @@
 /*   By: ericard@student.42.fr <ericard>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/05 14:22:04 by ericard@stu       #+#    #+#             */
-/*   Updated: 2021/03/16 15:19:32 by ericard@stu      ###   ########.fr       */
+/*   Updated: 2021/03/17 14:41:42 by ericard@stu      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,29 +22,29 @@ void	which_texture(t_infos *infos)
 		infos->tex.tex = 2;
 	if (infos->ray.side == 1 && infos->ray.raydiry >= 0)
 		infos->tex.tex = 3;
-	if (infos->ray.side == 1)
-		infos->tex.wallx = infos->ray.posy + infos->ray.perpwalldist * infos->ray.raydiry;
-	else
-		infos->tex.wallx = infos->ray.posx + infos->ray.perpwalldist * infos->ray.raydirx;
-	infos->tex.wallx -= floor((infos->tex.wallx));
 }
 
 void	print_texture(t_infos *infos, int i)
 {
 	which_texture(infos);
-	infos->tex.texx = (int)(infos->tex.wallx * (double)infos->textures[infos->tex.tex]. img_width);
+	if (infos->ray.side == 0)
+		infos->tex.wallx = infos->ray.posy + infos->ray.perpwalldist * infos->ray.raydiry;
+	else
+		infos->tex.wallx = infos->ray.posx + infos->ray.perpwalldist * infos->ray.raydirx;
+	infos->tex.wallx -= floor((infos->tex.wallx));
+	infos->tex.texx = (int)(infos->tex.wallx * (double)infos->textures[infos->tex.tex].img_width);
 	if (infos->ray.side == 0 && infos->ray.raydirx > 0)
 		infos->tex.texx = infos->textures[infos->tex.tex].img_width - infos->tex.texx - 1;
-	if (infos->ray.side == 1 && infos->ray.raydirx < 0)
+	if (infos->ray.side == 1 && infos->ray.raydiry < 0)
 		infos->tex.texx = infos->textures[infos->tex.tex].img_width - infos->tex.texx - 1;
+	infos->tex.step = 1.0 * infos->textures[infos->tex.tex].img_height / infos->ray.lineheight;
 	infos->tex.texpos = (infos->ray.drawstart - infos->resy / 2 + infos->ray.lineheight / 2) * infos->tex.step;
-	infos->tex.step = 1 * infos->textures[0].img_height / infos->ray.lineheight;
-	infos->tex.texpos = (infos->ray.drawstart - infos->resy / 2 + infos->ray.lineheight / 2) * infos->tex.step;
-	while (i <= infos->ray.drawend)
+	i = infos->ray.drawstart;
+	while (i < infos->ray.drawend)
 	{
 		infos->tex.texy = (int)infos->tex.texpos & (infos->textures[infos->tex.tex].img_height - 1);
 		infos->tex.texpos += infos->tex.step;
-		infos->mlx.addr[i * infos->mlx.line_length / 4 + infos->ray.x] = infos->textures[infos->tex.tex].addr[infos->tex.texy * infos->textures[infos->tex.tex].line_length / 4 + infos->tex.texx];
+		my_mlx_pixel_put(infos, infos->ray.x, i, infos->textures[infos->tex.tex].addr[infos->tex.texy * infos->textures[infos->tex.tex].img_height + infos->tex.texx]);
 		i++;
 	}
 }
@@ -52,15 +52,17 @@ void	print_texture(t_infos *infos, int i)
 void	print_columns(t_infos *infos)
 {
 	int i;
+	int j;
 
-	i = 0;
-	while (i < infos->ray.drawstart)
+	j = 0;
+	i = infos->ray.drawend;
+	while (j < infos->ray.drawstart)
 	{
-		my_mlx_pixel_put(infos, infos->ray.x, i, infos->c.value);
-		i++;
+		my_mlx_pixel_put(infos, infos->ray.x, j, infos->c.value);
+		j++;
 	}
-	if (i <= infos->ray.drawend)
-		print_texture(infos, i);
+	if (j <= infos->ray.drawend)
+		print_texture(infos, j);
 	/*while (i <= infos->ray.drawend)
 	{
 		if (infos->ray.side == 0 && infos->ray.raydirx < 0)
@@ -73,9 +75,10 @@ void	print_columns(t_infos *infos)
 			my_mlx_pixel_put(infos, infos->ray.x, i, 0x293847);
 		i++;
 	}*/
-	while (i < infos->resy)
+	j = i;
+	while (j < infos->resy)
 	{
-		my_mlx_pixel_put(infos, infos->ray.x, i, infos->f.value);
-		i++;
+		my_mlx_pixel_put(infos, infos->ray.x, j, infos->f.value);
+		j++;
 	}
 }
